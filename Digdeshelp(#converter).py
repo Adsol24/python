0d8eb6a640565a5501ec9ca2af49eb596d2daa44
Adsol24 <132016226+Adsol24@users.noreply.github.com>
2023-06-14 13:53:17 -0400

    Add files via upload

---
+++ Digdeshelp(#converter).py
@@ -0,0 +1,6 @@
+print("Digital Design help (number converter)")
+number = int((input("input the number you would like to convert: ")))
+
+print("your number in binary is {:b}".format(number))
+print("your number in octal is {:o}".format(number))
+print("your number in hexadecimal is {:X}".format(number))
